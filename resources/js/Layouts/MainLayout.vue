9b4cc6c7db2f989d398ec84118f8ade0780193d1
Kamil Coban <kamil.cobann.1@gmail.com>
2023-07-30 18:40:06 +0300

    Logout

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -9,14 +9,19 @@
                 <div class="text-xl text-indigo-600 dark:text-indigo-300 font-bold text-center">
                     <Link :href="route('listing.index')">LaraZillow</Link>
                 </div>
-                <div>
+                <div v-if="user" class="flex items-center gap-4">
+                    <div class="text-sm text-gray-500">{{ user.name }}</div>
                     <Link :href="route('listing.create')" class="button-primary">+ New Listing</Link>
+                    <Link :href="route('logout')" method="delete" as="button" class="text-sm text-gray-500 hover:text-gray-300" >Logout</Link>
+                </div>
+                <div v-else>
+                    <Link :href="route('login')">Log In</Link>
                 </div>
             </nav>
         </div>
     </header>
 
-    <main class="containter mx-auto p-4">
+    <main class="containter mx-auto p-4 w-full">
         <div v-if="flashSuccess" class="mb-4 border rounded-md shadow-sm border-green-200 dark:border-green-800 bg-green-50 dark:bg-green-900 p-2">
             {{ flashSuccess }}
         </div>
@@ -33,5 +38,9 @@ import { computed } from "vue";
 
 const flashSuccess = computed(
     () => usePage().props.flash.success
+)
+
+const user = computed(
+    () => usePage().props.user
 )
 </script>
